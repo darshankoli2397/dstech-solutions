37a34eee6c2eff2240673013aed578e243938da1
Darshan <darshankoli2397@gmail.com>
2022-05-09 13:08:47 +0530

    Updates

---
+++ precache-manifest.bd5f5e550b879357aa7ae4046ff637ae.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d89da2827fab2d09bf9660414577f0",
+    "url": "/dstech-solutions/index.html"
+  },
+  {
+    "revision": "bcff7262f643fd6a3749",
+    "url": "/dstech-solutions/static/css/2.f18effba.chunk.css"
+  },
+  {
+    "revision": "ee17089403796c879289",
+    "url": "/dstech-solutions/static/css/main.e81a1b15.chunk.css"
+  },
+  {
+    "revision": "bcff7262f643fd6a3749",
+    "url": "/dstech-solutions/static/js/2.d88e11dd.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/dstech-solutions/static/js/2.d88e11dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee17089403796c879289",
+    "url": "/dstech-solutions/static/js/main.92e857f3.chunk.js"
+  },
+  {
+    "revision": "e6c0d49d3637484db9b9",
+    "url": "/dstech-solutions/static/js/runtime-main.14aa7398.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/dstech-solutions/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/dstech-solutions/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/dstech-solutions/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/dstech-solutions/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/dstech-solutions/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/dstech-solutions/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/dstech-solutions/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/dstech-solutions/static/media/software.43049cae.jpeg"
+  },
+  {
+    "revision": "9085a48fc1c8066ad0dc4a6a5e2f886a",
+    "url": "/dstech-solutions/static/media/software1.9085a48f.png"
+  }
+]);
